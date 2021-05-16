f0da1ec13d6d47a9d1e073018854f855054e4c24
Azenga Kevin <azenga.kevin7@gmail.com>
2021-05-16 10:42:04 +0300

    Wip, hooking up tasks stuff to the existsing software

--- src/main/java/org/epics/controllers/taskManager/DashboardController.java
+++ src/main/java/org/epics/controllers/taskManager/DashboardController.java
@@ -1,7 +1,14 @@
 package org.epics.controllers.taskManager;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.epics.helpers.Log;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,9 +17,36 @@ public class DashboardController implements Initializable {
 
     public Button tasksButton;
     public Button inmatesButton;
+    public AnchorPane rootPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        tasksButton.setOnAction(event -> launchStage("/layouts/taskManager/Tasks.fxml", "Tasks | Prison Management Software"));
+        inmatesButton.setOnAction(event -> launchStage("/layouts/taskManager/Inmates.fxml", "Inmates | Prison Management Software"));
+    }
 
+
+    public void launchStage(String location, String title) {
+
+        try {
+
+            Parent parent = FXMLLoader.load(getClass().getResource(location));
+
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            Scene scene = new Scene(parent);
+
+            scene.getStylesheets().addAll(this.getClass().getResource("/styles/master.css").toExternalForm());
+
+            stage.setScene(scene);
+
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(rootPane.getScene().getWindow());
+
+            stage.show();
+
+        } catch (Exception exception) {
+            Log.error(getClass().getSimpleName(), "launchStage", exception);
+        }
     }
 }
